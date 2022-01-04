54e19778799512652879b34e9b44151d3fe2d48e
Kimi <miyu73@gmail.com>
2022-01-04 14:31:24 +0800

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'date-pipe';
+  dateToday: number = Date.now();
+  customDateTime = "Tue Oct 28 2025 23:28:00 GMT+0800";
 }
